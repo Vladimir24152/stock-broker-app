3fb353e1cfad4ddf4fde9631d3dd23c279619bb1
Malyshev Vladimir <mvs123331@gmail.com>
2025-11-03 19:14:28 +0400

    4 добавил SecurityConfig

---
+++ api-gateway/src/main/java/com/stockbroker/gateway/config/SecurityConfig.java
@@ -0,0 +1,37 @@
+package com.stockbroker.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/auth").permitAll()
+                        .requestMatchers("/api/all").hasRole("admin")
+                        .anyRequest().authenticated()
+                )
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .oauth2ResourceServer(resourceServer -> resourceServer
+                        .jwt(jwt -> jwt.jwtAuthenticationConverter(keycloakAuthConverter()))
+                );
+
+        return http.build();
+    }
+
+    private Converter<Jwt, ? extends AbstractAuthenticationToken> keycloakAuthConverter() {
+        return null;
+    }
+
+}
